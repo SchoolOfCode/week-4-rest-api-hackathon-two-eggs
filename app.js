58790bd8999d2ba21ac56c6f89001eef0046fe7c
Louis <178508612+L-Brookling@users.noreply.github.com>
2024-12-19 14:57:14 +0000

    Add MVP 1 progress

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+import { returnAllData, findPopulationByDate } from "./helperFunctions.js";
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/helperFunctions.js", async function (req, res) {
+  try {
+    const data = await returnAllData();
+    res.json({
+      success: true,
+      payload: data,
+    });
+  } catch (error) {
+    res.status(404).json({ error: "Could not retrieve data" });
+  }
+});
+
+app.listen(PORT, function () {
+  console.log(`Server is now listening on http://localhost:${PORT}`);
+});
+
+// response.json({
+//   success: true,
+//   payload: result, // Wrap the data in a `payload` object
+// });
